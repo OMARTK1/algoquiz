b952f1c07d5538f6df420e0219117cd4ee4ae034
OMARTK1 <elyazidi.omar@gmail.com>
2024-10-17 09:36:07 +0200

    Added Timer and AnswersReview Components with Related CSS

--- src/components/AnswersReview.js
+++ src/components/AnswersReview.js
@@ -1,26 +1,34 @@
 import React from 'react';
+import '../styles/AnswersReview.css';
 
 const AnswersReview = ({ questions, answers }) => {
+  console.log('Questions:', questions); // Debugging log
+  console.log('Answers:', answers);      // Debugging log
+
   return (
-    <div>
-      <h2>Review Your Answers</h2>
-      <ul>
+    <div className="container">
+      <h2 className="title">Review Your Answers</h2>
+      <ul className="question-list">
         {questions.map((question, index) => (
-          <li key={index}>
-            <h3>{question.question}</h3>
-            <ul>
+          <li key={index} className="question-item">
+            <h3 className="question">
+              {index + 1}. {question.question}
+            </h3>
+            <ul className="option-list">
               {question.options.map((option, optionIndex) => (
                 <li
                   key={optionIndex}
-                  style={{
-                    color: option === question.correct_answer
+                  className={`option-item ${
+                    option === question.correct_answer
                       ? 'green'
                       : answers[index] === option
                       ? 'red'
-                      : 'black',
-                  }}
+                      : 'black'
+                  }`}
                 >
                   {option}
+                  {option === question.correct_answer && " (Correct Answer)"}
+                  {answers[index] === option && option !== question.correct_answer && " (Your Answer)"}
                 </li>
               ))}
             </ul>
